db13fb1d1dc2a06c78adea4545043a525d422b0a
AntonAroche <anton.aroche@gmail.com>
2021-04-05 17:31:27 -0400

    Added clangd to lsp configuration

--- lua/modules/lsp.lua
+++ lua/modules/lsp.lua
@@ -2,3 +2,4 @@ local on_attach = require'completion'.on_attach
 require'lspconfig'.tsserver.setup{ on_attach=on_attach }
 require'lspconfig'.pyright.setup{on_attach=on_attach}
 require'lspconfig'.html.setup{on_attach=on_attach}
+require'lspconfig'.clangd.setup{on_attach=on_attach}
